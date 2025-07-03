9a5711364de6f4246443ac57cc6ec7b21b373f9c
mashaal <omar@teacups.io>
2025-07-03 18:04:54 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rm -f icky
 
-valac --pkg gtk4 --pkg gio-2.0 --pkg gio-unix-2.0 --pkg webkit2gtk-4.0 --pkg posix -o quicky quicky.vala
+valac --pkg gtk4 --pkg gio-2.0 --pkg gio-unix-2.0 --pkg webkitgtk-6.0 --pkg posix -o quicky quicky.vala
